a37e40dfd2ad3c3962ee38f84a2d0e547c158b41
Karen Nakahara <karenakahara@gmail.com>
2021-06-27 22:17:17 -0300

    config modal

---
+++ src/route.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const route = express.Router()
+
+route.get('/', (req, res) => res.render("index"))
+route.get('/room', (req, res) => res.render("room"))
+route.get('/create-pass', (req, res) => res.render("create-pass"))
+
+// Formato que o formulário de dentro da modal tem que passar a info
+// route.post('/room/:room/:question/:action')
+
+module.exports = route
+
