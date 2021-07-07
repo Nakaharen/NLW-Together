93d2b143f97b35a913441047d2b8d12a57c362cf
Karen Nakahara <karenakahara@gmail.com>
2021-07-07 11:56:07 -0300

    fix id room

--- src/controllers/room-controller.js
+++ src/controllers/room-controller.js
@@ -5,22 +5,33 @@ module.exports = {
     const db = await Database()
     const pass = req.body.password
     let roomId
+    let isRoom = true
 
-    for(let i = 0; i < 6; i++){
-      i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
-      roomId += Math.floor(Math.random() * 10).toString()
+    while(isRoom){
+      // Gera o número da sala
+      for(let i = 0; i < 6; i++){
+        i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
+        roomId += Math.floor(Math.random() * 10).toString()
+
+      // Verificar se esse número já existe
+      const roomsExistIds = await db.all(`SELECT id FROM rooms`)
+      isRoom = roomsExistIds.some(roomExistId => roomExistId === roomId)
+
+      if(! isRoom){
+        // Insere a sala no banco
+        await db.run(`INSERT INTO rooms (
+          id,
+          pass
+          ) VALUES(
+            ${parseInt(roomId)},
+            ${pass}
+            )`)        }
+      }
     }
-
-    await db.run(`INSERT INTO rooms (
-      id,
-      pass
-    ) VALUES(
-      ${parseInt(roomId)},
-      ${pass}
-    )`)
 
     await db.close()
 
     res.redirect(`/room/${roomId}`)
   }
 }
+
