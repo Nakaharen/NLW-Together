ce490601f5b144e5f4836c1164b3ff89171f88d8
Karen Nakahara <karenakahara@gmail.com>
2021-07-07 23:04:33 -0300

    adjust questions

--- src/controllers/room-controller.js
+++ src/controllers/room-controller.js
@@ -32,6 +32,17 @@ module.exports = {
     await db.close()
 
     res.redirect(`/room/${roomId}`)
+  },
+
+  async open(req, res) {
+    const db = await Database()
+    const roomId = req.params.room
+    const questions = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 0`)
+    const questionsRead = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 1`)
+
+    res.render("room", {roomId: roomId, questions: questions, questionsRead: questionsRead})
   }
 }
+
+
 
